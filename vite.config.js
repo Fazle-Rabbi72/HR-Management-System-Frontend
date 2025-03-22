ad11bbbc99022e11bbd9a491f480dfcee48fe624
Fazle-Rabbi72 <fazlerabbi0172291@gmail.com>
2025-03-22 11:16:14 -0600

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/",
-  server: {
+  plugins: [react(),
+    tailwindcss(),
+  ],
+  server:{
     port: 3000
-  },
-  build: {
-    chunkSizeWarningLimit: 1000, 
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    }
   }
 })
