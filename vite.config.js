3a5e9facaa0180fc1f0d126ef7c26086fc557c4f
Fazle-Rabbi72 <fazlerabbi0172291@gmail.com>
2025-03-22 11:22:25 -0600

    add chunk

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
   ],
-  server:{
+  server: {
     port: 3000
+  },
+  build: {
+    chunkSizeWarningLimit: 2000, // Set the chunk size warning limit (in KB)
   }
 })
