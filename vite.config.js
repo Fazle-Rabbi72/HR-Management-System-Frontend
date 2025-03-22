faeeba75a11b7d28fb34e4596fb755c9de18584d
Fazle-Rabbi72 <fazlerabbi0172291@gmail.com>
2025-03-22 11:37:31 -0600

    update vercel

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
+  plugins: [react(),
     tailwindcss(),
   ],
-  base:'/',
-  server: {
+  server:{
     port: 3000
-  },
-  build: {
-    chunkSizeWarningLimit: 2000, // Set the chunk size warning limit (in KB)
   }
 })
