cd955e157bf7707e5c6b22be4b47d5ca3e025ce4
Fazle-Rabbi72 <fazlerabbi0172291@gmail.com>
2025-03-22 10:53:45 -0600

    chunk limit increase

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
-  server:{
+  plugins: [react(), tailwindcss()],
+  server: {
     port: 3000
+  },
+  build: {
+    chunkSizeWarningLimit: 1000, 
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
   }
 })
